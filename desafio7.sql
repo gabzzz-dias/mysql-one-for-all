3cc9504040a3a1123a18d2b65487e8e75727572d
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-08 15:32:50 -0300

    req 7 again again again again

--- desafio7.sql
+++ desafio7.sql
@@ -3,11 +3,11 @@ SELECT a.artist_name AS artista,
 alb.album_name AS album,
 COUNT(uf.artist_id) AS seguidores
 FROM SpotifyClone.artists AS a
-INNER JOIN SpotifyClone.albums AS al
-ON a.artist_id = al.artist_id
+INNER JOIN SpotifyClone.albums AS alb
+ON a.artist_id = alb.artist_id
 INNER JOIN SpotifyClone.user_following AS uf
-ON al.artist_id = uf.artist_id
-GROUP BY al.album_id
+ON alb.artist_id = uf.artist_id
+GROUP BY alb.album_id
 ORDER BY seguidores DESC,
 artista ASC,
 album ASC;
