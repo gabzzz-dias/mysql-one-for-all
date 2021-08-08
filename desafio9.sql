92c5da382dec499ce5493b702be5813f37abe705
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-08 16:09:17 -0300

    req 9 again again

--- desafio9.sql
+++ desafio9.sql
@@ -1,14 +1,15 @@
 DELIMITER $$
 
-CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(100))
+CREATE PROCEDURE albuns_do_artista(IN name_artist VARCHAR(100))
 BEGIN
 
 SELECT	
 a.artist_name AS artista, al.album_name AS album
-FROM albums AS al
-INNER JOIN artistas AS a ON al.artist_id = a.artist_id
-WHERE a.artist_name = artista
-ORDER BY al.album_name;
+FROM SpotifyClone.artists AS a
+INNER JOIN SpotifyClone.albums AS al
+WHERE a.artist_id = al.artist_id
+AND a.artist_name = name_artist
+ORDER BY album ASC;
 
 END $$
 DELIMITER ;
