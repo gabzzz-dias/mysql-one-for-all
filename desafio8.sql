d32999e5bd41dcf84fbee9d39d3beae52c373d62
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-08 14:55:34 -0300

    req 8

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE
+ON usuarios FOR EACH ROW
+BEGIN
+
+DELETE FROM historico_de_reproducoes
+WHERE historico_de_reproducoes.usuario_id = OLD.usuario_id;
+DELETE FROM seguindo_artistas
+WHERE seguindo_artistas.usuario_id = OLD.usuario_id;
+
+END$$    
+
+DELIMITER ;
