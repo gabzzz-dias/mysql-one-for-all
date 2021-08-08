896b66d7985536f7e8eecc5f3bdeb59fd3fb7db2
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-08 14:52:57 -0300

    fix req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,13 +1,13 @@
 CREATE VIEW perfil_artistas AS
-SELECT 
-a.artist_name AS `artista`,
-alb.album_name AS `album`,
-(SELECT COUNT(*) FROM SpotifyClone.user_following
-WHERE artist_id = a.artist_id) AS `seguidores`
-FROM
-SpotifyClone.albums AS alb
-INNER JOIN artistas AS a
-ON alb.artist_id = a.artist_id
+SELECT a.artist_name AS 'artista',
+alb.album_name AS 'album',
+COUNT(uf.artist_id) AS 'seguidores'
+FROM SpotifyClone.artists AS a
+INNER JOIN SpotifyClone.albums AS al
+ON a.artist_id = al.artist_id
+INNER JOIN SpotifyClone.user_following AS uf
+ON al.artist_id = uf.artist_id
+GROUP BY al.album_id
 ORDER BY `seguidores` DESC,
 `artista` ASC,
 `album` ASC;
